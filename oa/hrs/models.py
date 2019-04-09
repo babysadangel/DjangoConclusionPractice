7a7cdffc1b18c5992064a3b719e5275be1800811
babysadangel <yuankisstherain@163.com>
2019-04-09 16:14:48 +0800

    practice

--- oa/hrs/models.py
+++ oa/hrs/models.py
@@ -1,35 +1,29 @@
 from django.db import models
 
-# ORM - 对象关系映射
-# 对象模型  <--->   关系模型
-# 实体类    <--->   二维表
-# 属性      <--->   列
-# 对象      <--->   记录
+class  Dept(models.Model):
 
-
-class Dept(models.Model):
-    no = models.IntegerField(db_column='deptno', primary_key=True, verbose_name='部门编号')
-    name = models.CharField(db_column='dname', max_length=20, verbose_name='部门名称')
-    location = models.CharField(db_column='dloc', max_length=10, verbose_name='部门所在地')
-    # excellent = models.BooleanField(default=0, verbose_name='是否优秀')
-
-    def __str__(self):
-        return self.name
+    #部门类
+    no = models.Integer(primary_key = True, db_column = 'dno',verbose_name = '部门编号')
+    name = models.CharField(max_length =20, db_column='dname', verbose_name='部门名称')
+    location  =models.CharField(max_length=10, db_column='dloc', verbose_name='部门所在地')
 
     class Meta:
-        db_table = 'TbDept'
+        db_table = 'tb_dept'
 
 
-class Emp(models.Model):
-    no = models.IntegerField(db_column='empno', primary_key=True)
-    name = models.CharField(db_column='ename', max_length=20)
-    job = models.CharField(db_column='job', max_length=10)
-    # mgr = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
-    mgr = models.IntegerField(null=True, blank=True)
-    sal = models.DecimalField(max_digits=7, decimal_places=2)
-    comm = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    dept = models.ForeignKey(Dept, db_column='dno', on_delete=models.PROTECT)
+class emp(models.Model):
+    #员工类
+    no = models.IntegerField(primary_key=True, db_colum='eno', verbose_name='员工编号')
+    name = models.CharField(max_length=20, db_column='ename', verbose_name='员工姓名')
+    job = models.CharField(max_length = 10,verbose_name='职位')
+
+    #自参照完整性多对一键关联
+    mgr = models.ForeignKey('self', on_delete=models.SET_NULL,null=True, blank=True,verbose_name='主管编号')
+    sal = models.DecimalField(max_digits=7,decimal_place=2, verbose_name='月薪')
+    comm = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True,verbose_name='补贴')
+    #多对一外键关联
+    dept  =models.ForeignKey(Dept, db_column='don', on_delete=models.PROTECT, verbose_name='所在部门')
 
     class Meta:
-        db_table = 'TbEmp'
+        db_table = 'tb_emp'
 
---
+++ oa/sql/oa.sql
@@ -0,0 +1,2 @@
+drop database if exists oa;
+create database oa default charset utf8;
