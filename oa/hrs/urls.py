2197627a9c743325ce6a89e6b810323f849311ad
babysadangel <yuankisstherain@163.com>
2019-04-10 19:05:09 +0800

    数据库

---
+++ oa/hrs/urls.py
@@ -0,0 +1,10 @@
+
+
+from  django.urls import  path
+
+from  hrs import  views
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+]
+
---
+++ oa/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/administrator/Documents/DjangoConclusionPractice/oa/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
